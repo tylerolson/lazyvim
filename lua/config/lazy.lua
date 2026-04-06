a5946fd4016740ed8121f6c299f400ef9bb705f8
Tyler Olson <tolson519@gmail.com>
2026-04-05 22:34:45 -0700

    add more themes

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -17,9 +17,7 @@ vim.opt.rtp:prepend(lazypath)
 require("lazy").setup({
   spec = {
     -- add LazyVim and import its plugins
-    { "LazyVim/LazyVim", import = "lazyvim.plugins", opts = {
-      colorscheme = "catppuccin-macchiato",
-    } },
+    { "LazyVim/LazyVim", import = "lazyvim.plugins" },
     -- import/override with your plugins
     { import = "plugins" },
   },
@@ -32,7 +30,7 @@ require("lazy").setup({
     version = false, -- always use the latest git commit
     -- version = "*", -- try installing the latest stable version for plugins that support semver
   },
-  install = { colorscheme = { "catppuccin" } },
+  install = {},
   checker = {
     enabled = true, -- check for plugin updates periodically
     notify = false, -- notify on update
---
+++ lua/plugins/theme.lua
@@ -0,0 +1,11 @@
+return {
+  { "neanias/everforest-nvim" },
+  { "sainnhe/gruvbox-material" },
+  { "sainnhe/sonokai" },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "everforest",
+    },
+  },
+}
