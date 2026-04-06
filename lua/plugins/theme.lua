9fe7374e4b55745fa88303452aee93e44b0a399b
Tyler Olson <tolson519@gmail.com>
2026-04-06 03:20:54 -0700

    change SnacksPickerPathHidden to a lighter color for better contrast

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,7 +1,21 @@
 return {
-  { "neanias/everforest-nvim" },
-  { "sainnhe/gruvbox-material" },
-  { "sainnhe/sonokai" },
+  { "sainnhe/gruvbox-material", lazy = false, priority = 1000 },
+  { "sainnhe/sonokai", lazy = false, priority = 1000 },
+  {
+    "neanias/everforest-nvim",
+    version = false,
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require("everforest").setup({
+        background = "hard",
+        ui_contrast = "high",
+        on_highlights = function(hl, palette)
+          hl.SnacksPickerPathHidden = { fg = palette.grey1 }
+        end,
+      })
+    end,
+  },
   {
     "LazyVim/LazyVim",
     opts = {
